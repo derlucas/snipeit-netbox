500cb016127b456e3af5da1ac22de114f98e637b
Folke Ashberg <folke@ashberg.de>
2023-10-28 13:31:21 +0200

    Bugfixes and Better Update Handling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 import snipe
 import pynetbox
 import syncer
-
+import sys
 
 if __name__ == "__main__":
 
@@ -21,23 +21,28 @@ if __name__ == "__main__":
     snipe = snipe.Snipe(config['config']['snipe_url'], config['config']['snipe_token'])
     netbox = pynetbox.api(config['config']['netbox_url'], config['config']['netbox_token'])
 
+    logging.info("Checking Netbox Custom Fields")
     syncer = syncer.Syncer(netbox, snipe, args.allow_update, args.allow_linking)
     syncer.ensure_netbox_custom_field(False)
 
+    logging.info("Syncing Companies")
     snipe_companies = snipe.get_companies()
     syncer.sync_companies_to_tenants(snipe_companies)
 
+    logging.info("Syncing Manufacturers and Models with MACs")
     snipe_manufacturers, snipe_models = snipe.get_models_and_manufacturers_with_mac()
     syncer.sync_manufacturers(snipe_manufacturers)
     syncer.sync_models_to_device_types(snipe_models)
 
+    logging.info("Syncing Locations")
     locations = snipe.get_locations()
     syncer.sync_top_locations_to_sites(locations)
     syncer.sync_locations(locations)
 
-
+    logging.info("Syncing Assets with MACs")
     assets = snipe.get_assets_with_mac()
     syncer.sync_assets_to_devices(assets)
     # for asset in assets:
     #     print("{} {}".format(asset['asset_tag'], asset['name']))
 
+    sys.exit()
